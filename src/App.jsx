c04bacdd29a3d85448b47d789145ade21f11a110
BlindDreamer <blinddreaemr@huku.rocks>
2024-09-30 01:13:24 +0300

    fix duplicate on 404

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Monitor from "./components/monitorpage";
 import Home from "./components/home";
 import PageNotFound from "./components/404";
 import { Route, Link, Routes, useLocation } from "react-router-dom";
-import PageNotFound from "./assets";
 
 const App = () => {
   const location = useLocation();
--- src/components/404.jsx
+++ src/components/404.jsx
@@ -3,10 +3,10 @@ import Animated from "./animated";
 const PageNotFound = () => {
   return (
     <Animated>
-      <Fragment>
+      <>
         <h1>404</h1>
         <h2>page not found</h2>
-      </Fragment>
+      </>
     </Animated>
   );
 };
