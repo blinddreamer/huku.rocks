083b62d49b75a424a4356cdb00364c95b69c1009
BlindDreamer <blinddreaemr@huku.rocks>
2026-01-01 00:52:53 +0200

    remove monitor

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import Video from "./components/video";
 import Footer from "./components/footer";
 import { AnimatePresence } from "framer-motion";
-import Monitor from "./components/monitorpage";
 import Home from "./components/home";
 import PageNotFound from "./components/404";
 import { Route, Link, Routes, useLocation } from "react-router-dom";
@@ -13,13 +12,11 @@ const App = () => {
       <div class="container">
         <div class="flex-navbar">
           <Link to="/">home</Link>
-          <Link to="/monitor">monitor</Link>
         </div>
         <div class="flex-content">
           <AnimatePresence initial={false} mode={"wait"}>
             <Routes location={location} key={location.pathname}>
               <Route path="/" element={<Home />} />
-              <Route path="/monitor" element={<Monitor />} />
               <Route path="*" element={<PageNotFound />} />
             </Routes>
           </AnimatePresence>
--- src/components/monitor.jsx
+++ src/components/monitor.jsx
@@ -1,3 +1,5 @@
+//DO NOT USE
+
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
